8be8354a38b5e9fe836e971e901fa22a266e77af
Adarsh Kumar <adarshk2k6@gmail.com>
2025-07-26 16:28:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-from local_str import result
-from 
+from local_str import keywords
+from suggests import Suggestion
+
+def main() -> None:
+  print("Enter Space or Tab to Exit\n")
+  inp = input("Enter String: ")
+
+  suggestor = 
+
+  while inp not in [' ', '\t']:
+    res = suggestor.suggestions(inp)
+    for item in res: print(f"\t{item}")
